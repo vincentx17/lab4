7b9a8ba1f62a38ec7fbc8f31b8307440476818a8
vagrant <vagrant@precise32.(none)>
2015-01-29 23:20:14 +0000

    node.js lab

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 /**
- * Module dependencies.
- */
+ * Module dependencies
+. */
 
 var express = require('express');
 var http = require('http');
@@ -11,6 +11,7 @@ var handlebars = require('express3-handlebars')
 var index = require('./routes/index');
 // Example route
 // var user = require('./routes/user');
+var project = require('./routes/project');
 
 var app = express();
 
@@ -38,6 +39,8 @@ if ('development' == app.get('env')) {
 app.get('/', index.view);
 // Example route
 // app.get('/users', user.list);
+app.get('/project', project.viewProject);
+app.get('/project/:name', project.viewProject);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
